56f5db7ed473853b1d3f9b21ebe9912f68e6a6ad
Isaacgv <isaacgonzalesv@gmail.com>
2023-10-22 12:08:21 +0200

    modify Footer ans Header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,14 +1,48 @@
 <script setup lang="ts">
-
 </script>
 
 <template>
-    <div>
-        <h1>Header</h1>
-    </div>
+    <header class="px-20 d-flex flex-row align-items-center">
+        <a href="#" class="d-flex flex-row align-items-center mr-20">
+            <img srt="../assets/logo.svg" />
+            <span class="logo">IG</span>
+        </a>
+        <ul class="d-flex flex-row flex-fill">
+            <li class="mr-10">
+                <a href="#">Shop</a>
+            </li>
+            <li>
+                <a href="#">Admin</a>
+            </li>
+        </ul>
+        <ul class="d-flex flex-row ">
+            <li class="mr-10">
+                <a href="#">Inscription</a>
+            </li>
+            <li>
+                <a href="#">Connexion</a>
+            </li>
+
+        </ul>
+    </header>
   
 </template>
 
 <style lang="scss" scoped>
+    header {
+        background-color: var(--primary-1);
+        a{
+            color: var(--text-primary-color);
+            img {
+                width: 20px;
+                margin-right: 5px;
+            }
+            .logo {
+                font-weight: 700;
+                font-size: 20px;
+            }
+            
+        }
+    }
 
 </style>
